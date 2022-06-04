298d9ce5d378e3b4455e5e922bc2ae0bb4092293
regal-rahul <regalrahul777@gmail.com>
2022-06-04 11:21:19 +0530

    Added dummy cities data and camp seed helpers

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/go-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -8,6 +23,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My First backyard', description: 'Cheap camping!' });
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
