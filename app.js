0a59bfd2f1340ea7a44580468fa31345e93c9c93
regal-rahul <regalrahul777@gmail.com>
2022-06-09 12:39:32 +0530

    codesnap

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -51,9 +52,10 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 app.post('/campgrounds', catchAsync(async (req, res) => {
-        const campground = new Campground(req.body.campground);
-        await campground.save();
-        res.redirect(`/campgrounds/${campground._id}`)
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 
@@ -78,9 +80,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 
 app.use((err, req, res, next) => {
-    res.send('Oh Boy Something went Wrong!')
+    const { statusCode = 500, message = "Something went Wrong!" } = err;
+    res.status(statusCode).send(message);
 });
 
 
