56e4b092258fc45944121e01f54d753e25ac117d
regal-rahul <regalrahul777@gmail.com>
2022-06-14 17:08:55 +0530

    served public folder

--- app.js
+++ app.js
@@ -35,9 +35,11 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-
+app.use(express.static('public'));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews' ,reviews);
