655e9301f920ea8217b0f5b53c76b2f80b520778
regal-rahul <regalrahul777@gmail.com>
2022-06-14 18:12:12 +0530

    Added error flash

--- routes/campground.js
+++ routes/campground.js
@@ -48,6 +48,10 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that Campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground })
 }));
 
