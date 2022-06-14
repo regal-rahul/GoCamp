dfd7718c72722654bc4ca70b3ef6affc2454cc77
regal-rahul <regalrahul777@gmail.com>
2022-06-14 15:44:37 +0530

    restructured things and up and running

--- routes/campground.js
+++ routes/campground.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError');
-const Campground = require('./models/campground');
+const catchAsync = require('../utils/catchAsync');
+const { campgroundSchema } = require('../schemas.js');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
 
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
 
 
 
