7ae81c6eac3fc2e5ae6669e192b1d22e39e3ca39
regal-rahul <regalrahul777@gmail.com>
2022-06-17 21:17:52 +0530

    add register post route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
+router.post('/register', catchAsync(async(req, res) => {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('success', 'Welcome to Go Camp!');
+    res.redirect('/campgrounds');
+}));
 
 module.exports = router;
