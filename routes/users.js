1af656b58b0a549a5a5280f6ff5c076034c9cfd2
regal-rahul <regalrahul777@gmail.com>
2022-06-17 21:37:41 +0530

    add login get and post route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -7,12 +8,24 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 router.post('/register', catchAsync(async(req, res) => {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('success', 'Welcome to Go Camp!');
-    res.redirect('/campgrounds');
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Go Camp!');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
