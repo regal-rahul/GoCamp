df786aa18b8819cba499c9a2773f680a4af3c37e
regal-rahul <regalrahul777@gmail.com>
2022-06-18 20:11:47 +0530

    Refactoring and restructuring code

--- routes/campground.js
+++ routes/campground.js
@@ -5,20 +5,39 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 
+router.route('/')
+    .get( catchAsync(campgrounds.index))
+    .post(
+        isLoggedIn,
+        validateCampground,
+        catchAsync(campgrounds.createCampground)
+    );
 
-router.get('/', catchAsync(campgrounds.index));
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.get('/new',
+    isLoggedIn,
+    campgrounds.renderNewForm
+);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get( catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn,
+        isAuthor,
+        validateCampground,
+        catchAsync(campgrounds.updateCampground)
+    )
+    .delete(
+        isLoggedIn,
+        isAuthor,
+        catchAsync(campgrounds.deleteCampground)
+    );
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.get('/:id/edit',
+    isLoggedIn,
+    isAuthor,
+    catchAsync(campgrounds.renderEditForm)
+);
 
 
 module.exports = router;
