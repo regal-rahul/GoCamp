4eb8ceac8c187bbcf533fbe85e38e2c12473630c
regal-rahul <regalrahul777@gmail.com>
2022-06-18 13:40:28 +0530

    create camp having author

--- app.js
+++ app.js
@@ -68,11 +68,6 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({email: 'a@b.com', username: 'abc'});
-    const newUser = await User.register(user, 'chicken');
-    res.send(newUser);
-})
 
 
 app.use('/', userRoutes);
