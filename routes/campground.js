465430a0b3f1bd4d0645470a3b8f43e1d8217449
regal-rahul <regalrahul777@gmail.com>
2022-06-18 13:40:00 +0530

    author route

--- routes/campground.js
+++ routes/campground.js
@@ -33,6 +33,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made new Campground!');
     res.redirect(`/campgrounds/${campground._id}`)
@@ -40,7 +41,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => 
 
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that Campground!');
         return res.redirect('/campgrounds');
