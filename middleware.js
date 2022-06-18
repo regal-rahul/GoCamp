197feca11b24de6ea09bf874d898f7cfe6b3f54c
regal-rahul <regalrahul777@gmail.com>
2022-06-18 19:21:09 +0530

    Refactoring code

--- middleware.js
+++ middleware.js
@@ -34,8 +34,8 @@ module.exports.isAuthor = async(req, res, next) => {
 }
 
 module.exports.isReviewAuthor = async(req, res, next) => {
-    const { reviewid } = req.params;
-    const review = await Review.findById(reviewid);
+    const {id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
