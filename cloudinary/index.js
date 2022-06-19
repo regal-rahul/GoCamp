78af6b94a2223a9c470d7743f72330bd6bffa365
regal-rahul <regalrahul777@gmail.com>
2022-06-19 12:57:41 +0530

    cloud upload tested

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -4,18 +4,20 @@ const express = require('express');
 const multer = require('multer');
  
 const app = express();
- 
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'some-folder-name',
-    format: async (req, file) => 'png', // supports promises as well
-    public_id: (req, file) => 'computed-filename-using-request',
-  },
+  cloudinary,
+    folder: 'go-camp',
+    allowedFormats: ['jpeg', 'png', 'jpg']
 });
  
-const parser = multer({ storage: storage });
- 
-app.post('/upload', parser.single('image'), function (req, res) {
-  res.json(req.file);
-});
+module.exports = {
+    cloudinary,
+    storage
+}
--- routes/campground.js
+++ routes/campground.js
@@ -5,7 +5,8 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 const multer = require('multer');
-const upload = multer({ dest: 'upload/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get( catchAsync(campgrounds.index))
