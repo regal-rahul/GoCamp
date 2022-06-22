44fff29120166b7d4fd6c53114033fe06826f871
regal-rahul <regalrahul777@gmail.com>
2022-06-22 16:48:39 +0530

    added security

--- app.js
+++ app.js
@@ -1,61 +1,59 @@
-if(process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
+const { error } = require("console");
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-const { error } = require('console');
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campground");
+const reviewRoutes = require("./routes/reviews");
 
-
-const userRoutes = require('./routes/users');
-const campgroundRoutes = require('./routes/campground');
-const reviewRoutes = require('./routes/reviews');
-
-
-mongoose.connect('mongodb://localhost:27017/go-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost:27017/go-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
-
 
 const app = express();
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
-
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
 const sessionConfig = {
-    secret: 'thisshloudbeabettersecret!',
-    resave: false,
-    saveUnintialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  secret: "thisshloudbeabettersecret!",
+  resave: false,
+  saveUnintialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
@@ -65,36 +63,32 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
-
-
-
-app.use('/', userRoutes);
-app.use('/campgrounds', campgroundRoutes);
-app.use('/campgrounds/:id/reviews' ,reviewRoutes);
-
-app.get('/', (req, res) => {
-    res.render('home');
+  console.log(req.query);
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "Something went Wrong!" } = err;
-    if (!err.message) err.message = 'Oh No, Something went Wrong!';
-    res.status(statusCode).render('error', { err });
+  const { statusCode = 500, message = "Something went Wrong!" } = err;
+  if (!err.message) err.message = "Oh No, Something went Wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
-
 app.listen(3000, () => {
-    console.log('Serving on port 3000!')
+  console.log("Serving on port 3000!");
 });
